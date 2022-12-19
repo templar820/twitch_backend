2a28faa29f08bcc5a25a10fe168b872601a5c7e3
Ivan Kuznetsov <i_kuznetsov@lesta.group>
2022-12-19 14:30:25 +0300

    q2we

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverless = require('serverless-http');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = '.netlify/functions/main';
-  app.setGlobalPrefix(globalPrefix);
-  await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverless(expressApp);
+  await app.listen(3000);
 }
-let server;
-export const handler = async (event, context, callback) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+bootstrap();
